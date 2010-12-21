1c2709f2da87e81f400af654fe0452c3df2fcbaf
mktrost <mktrost@umich.edu>
2010-12-21 14:24:53 -0500

    BAM - fix tests for the renaming of the sequence translation options.

--- src/bam/test/testSeq.sh
+++ src/bam/test/testSeq.sh
@@ -3,38 +3,38 @@ ERROR=false
 # Sam To Sam
 ../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqNoOpt.sam && diff results/seqNoOpt.sam expected/seqOrig.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqOrig.sam --seqOrig && diff results/seqOrig.sam expected/seqOrig.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqOrig.sam --useOrigSeq && diff results/seqOrig.sam expected/seqOrig.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqBases.sam --seqBases && diff results/seqBases.sam expected/seqBases.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqBases.sam --useBases && diff results/seqBases.sam expected/seqBases.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqEquals.sam --seqEquals && diff results/seqEquals.sam expected/seqEquals.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqEquals.sam --useEquals && diff results/seqEquals.sam expected/seqEquals.sam \
 && \
 # Bam to Sam
 ../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqNoOptBam.sam && diff results/seqNoOptBam.sam expected/seqOrig.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqOrigBam.sam --seqOrig && diff results/seqOrigBam.sam expected/seqOrig.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqOrigBam.sam --useOrigSeq && diff results/seqOrigBam.sam expected/seqOrig.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqBasesBam.sam --seqBases && diff results/seqBasesBam.sam expected/seqBases.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqBasesBam.sam --useBases && diff results/seqBasesBam.sam expected/seqBases.sam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqEqualsBam.sam --seqEquals && diff results/seqEqualsBam.sam expected/seqEquals.sam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqEqualsBam.sam --useEquals && diff results/seqEqualsBam.sam expected/seqEquals.sam \
 && \
 # Sam To Bam
 ../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqNoOpt.bam && diff results/seqNoOpt.bam expected/seqOrig.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqOrig.bam --seqOrig && diff results/seqOrig.bam expected/seqOrig.bam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqOrig.bam --useOrigSeq && diff results/seqOrig.bam expected/seqOrig.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqBases.bam --seqBases && diff results/seqBases.bam expected/seqBases.bam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqBases.bam --useBases && diff results/seqBases.bam expected/seqBases.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqEquals.bam --seqEquals && diff results/seqEquals.bam expected/seqEquals.bam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.sam --out results/seqEquals.bam --useEquals && diff results/seqEquals.bam expected/seqEquals.bam \
 && \
 # Bam To Bam
 ../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqNoOptBam.bam && diff results/seqNoOptBam.bam expected/seqOrig.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqOrigBam.bam --seqOrig && diff results/seqOrigBam.bam expected/seqOrig.bam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqOrigBam.bam --useOrigSeq && diff results/seqOrigBam.bam expected/seqOrig.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqBasesBam.bam --seqBases && diff results/seqBasesBam.bam expected/seqBases.bam \
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqBasesBam.bam --useBases && diff results/seqBasesBam.bam expected/seqBases.bam \
 && \
-../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqEqualsBam.bam --seqEquals && diff results/seqEqualsBam.bam expected/seqEquals.bam
+../../bin/bam convert --refFile testFiles/chr1_partial.fa --in testFiles/testFilter.bam --out results/seqEqualsBam.bam --useEquals && diff results/seqEqualsBam.bam expected/seqEquals.bam
 
 if [ $? -ne 0 ]
 then
