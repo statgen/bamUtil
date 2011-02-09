41b77e9904eac08e60626a97d88f2752e377d144
mktrost <mktrost@umich.edu>
2011-02-09 16:48:00 -0500

    Clean up spacing.

--- src/mgpileup/Main.cpp
+++ src/mgpileup/Main.cpp
@@ -35,10 +35,10 @@
 #include "InputFile.h"
 
 int main(int argc, char ** argv)
-{			    
-	try 
-	{
-		std::string desc = "Example:\n\
+{
+    try 
+    {
+        std::string desc = "Example:\n\
 ./gpileup -r /data/local/ref/karma.ref/human.g1k.v37 -b ../data/HG00160.chrom20.ILLUMINA.bwa.GBR.low_coverage.20100517.bam -v HG00160.chrom20.ILLUMINA.bwa.GBR.low_coverage.20100517.vcf -i -i ../data/LDL_b37.modified.genos.EUR.vcf -d \n\
 Pileup takes in a BAM file and a genome reference file to return the following \n\
 Alignment statistics are written to the VCF file specified, if the file name ends with .gz, the file output will be in gzip format.\n\
@@ -60,86 +60,86 @@ Alignment statistics are written to the VCF file specified, if the file name end
    g. GL        : genotype likelihood scores - AA,AC,AG,AT,CC,CG,CT,GG,GT,TT.\n\
 10. <vcf output file name> : contains data described in FORMAT.\n";
    			
-   		std::string version = "0.577";
-		TCLAP::CmdLine cmd(desc, ' ', version);
-		TCLAP::ValueArg<std::string> argInputBAMFileName("b", "bam", "BAM file", true, "", "string");
-		TCLAP::ValueArg<std::string> argRefSeqFileName("r", "reference", "Reference Sequence file", true, "", "string");
-		TCLAP::ValueArg<std::string> argInputVCFFileName("i", "inputvcf", "VCF file listing the loci of interest (can be gzipped), bam index file is automatically assumed to be in the same location as the bam file.", false, "", "string");
-		TCLAP::ValueArg<std::string> argOutputVCFFileName("v", "ouputvcf", "VCF file - if the extension is .gz, the written file will be a gzip file, (default is STDOUT)", false, "-", "string");
-		TCLAP::SwitchArg argAddDelAsBase("d", "adddelasbase", "Adds deletions as base", cmd, false);
+        std::string version = "0.577";
+        TCLAP::CmdLine cmd(desc, ' ', version);
+        TCLAP::ValueArg<std::string> argInputBAMFileName("b", "bam", "BAM file", true, "", "string");
+        TCLAP::ValueArg<std::string> argRefSeqFileName("r", "reference", "Reference Sequence file", true, "", "string");
+        TCLAP::ValueArg<std::string> argInputVCFFileName("i", "inputvcf", "VCF file listing the loci of interest (can be gzipped), bam index file is automatically assumed to be in the same location as the bam file.", false, "", "string");
+        TCLAP::ValueArg<std::string> argOutputVCFFileName("v", "ouputvcf", "VCF file - if the extension is .gz, the written file will be a gzip file, (default is STDOUT)", false, "-", "string");
+        TCLAP::SwitchArg argAddDelAsBase("d", "adddelasbase", "Adds deletions as base", cmd, false);
 
-		cmd.add(argInputBAMFileName);
-		cmd.add(argRefSeqFileName);
-		cmd.add(argInputVCFFileName);
-		cmd.add(argOutputVCFFileName);
-		cmd.parse(argc, argv);
+        cmd.add(argInputBAMFileName);
+        cmd.add(argRefSeqFileName);
+        cmd.add(argInputVCFFileName);
+        cmd.add(argOutputVCFFileName);
+        cmd.parse(argc, argv);
 
-		std::cout << "Running gpileup version " << version << std::endl; 
-		std::string inputBAMFileName = argInputBAMFileName.getValue();
-		std::cout << "bam file                : " << inputBAMFileName << std::endl; 
-		std::string refSeqFileName = argRefSeqFileName.getValue();	
-		std::cout << "reference sequence file : " << argRefSeqFileName.getValue() << std::endl; 
-		std::string inputVCFFileName = argInputVCFFileName.getValue();
-		//if input VCF is detected, look for BAM index file
-		std::string inputBAMIndexFileName = "";
-		if (inputVCFFileName != "")
-		{
-			std::cout << "input VCF file          : " << inputVCFFileName << std::endl; 
-			if (inputBAMFileName.length()>4 && (inputBAMFileName.substr(inputBAMFileName.length()-4, 4) == ".bam"))
-			{
-				inputBAMIndexFileName.append(inputBAMFileName.c_str());
-		    	inputBAMIndexFileName.append(".bai");
-			}	
-			std::cout << "bam index file          : " << inputBAMIndexFileName << std::endl; 
-		}
-		std::string outputVCFFileName = argOutputVCFFileName .getValue();
+        std::cout << "Running gpileup version " << version << std::endl; 
+        std::string inputBAMFileName = argInputBAMFileName.getValue();
+        std::cout << "bam file                : " << inputBAMFileName << std::endl; 
+        std::string refSeqFileName = argRefSeqFileName.getValue();	
+        std::cout << "reference sequence file : " << argRefSeqFileName.getValue() << std::endl; 
+        std::string inputVCFFileName = argInputVCFFileName.getValue();
+        //if input VCF is detected, look for BAM index file
+        std::string inputBAMIndexFileName = "";
+        if (inputVCFFileName != "")
+        {
+            std::cout << "input VCF file          : " << inputVCFFileName << std::endl; 
+            if (inputBAMFileName.length()>4 && (inputBAMFileName.substr(inputBAMFileName.length()-4, 4) == ".bam"))
+            {
+                inputBAMIndexFileName.append(inputBAMFileName.c_str());
+                inputBAMIndexFileName.append(".bai");
+            }	
+            std::cout << "bam index file          : " << inputBAMIndexFileName << std::endl; 
+        }
+        std::string outputVCFFileName = argOutputVCFFileName .getValue();
 
-		bool inputVCFFileIsGZipped = false;
-		if (outputVCFFileName.length()>3 && (outputVCFFileName.substr(outputVCFFileName.length()-3, 3) == ".gz"))
-		{
-			inputVCFFileIsGZipped = true;
-		}
-		else
-		{
-			inputVCFFileIsGZipped = false;
-		}
-	
-		bool outputVCFFileIsGZipped = false;
-		if (outputVCFFileName.length()>3 && (outputVCFFileName.substr(outputVCFFileName.length()-3, 3) == ".gz"))
-		{
-	    	std::cout << "output VCF file         : " << outputVCFFileName << " (gzip)" << std::endl; 
-			outputVCFFileIsGZipped = true;
-		}
-		else if (outputVCFFileName=="-")
-		{
-			std::cout << "output VCF file         : STDOUT" << std::endl; 
-			outputVCFFileIsGZipped = false;
-		}
-		else
-		{
-			std::cout << "output VCF file         : " << outputVCFFileName << std::endl; 
-			outputVCFFileIsGZipped = false;
-		}
+        bool inputVCFFileIsGZipped = false;
+        if (outputVCFFileName.length()>3 && (outputVCFFileName.substr(outputVCFFileName.length()-3, 3) == ".gz"))
+        {
+            inputVCFFileIsGZipped = true;
+        }
+        else
+        {
+            inputVCFFileIsGZipped = false;
+        }
 
-		std::cout << "add deletions as bases  : " << (argAddDelAsBase.getValue()? "yes" : "no") << std::endl; 
+        bool outputVCFFileIsGZipped = false;
+        if (outputVCFFileName.length()>3 && (outputVCFFileName.substr(outputVCFFileName.length()-3, 3) == ".gz"))
+        {
+            std::cout << "output VCF file         : " << outputVCFFileName << " (gzip)" << std::endl; 
+            outputVCFFileIsGZipped = true;
+        }
+        else if (outputVCFFileName=="-")
+        {
+            std::cout << "output VCF file         : STDOUT" << std::endl; 
+            outputVCFFileIsGZipped = false;
+        }
+        else
+        {
+            std::cout << "output VCF file         : " << outputVCFFileName << std::endl; 
+            outputVCFFileIsGZipped = false;
+        }
 
-		PileupWithGenomeReference<PileupElementBaseQual> pileup(1024, refSeqFileName, argAddDelAsBase.getValue(), inputVCFFileIsGZipped, outputVCFFileIsGZipped);   	
-		
-		//process file with index    	
-		if (inputVCFFileName != "")
-		{
-    		pileup.processFile(inputBAMFileName, inputBAMIndexFileName, inputVCFFileName, outputVCFFileName);
-		}
-		else
-		{
-    		pileup.processFile(inputBAMFileName, outputVCFFileName);
-		}
-	}
-	catch (TCLAP::ArgException &e) 
-	{
-		std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
-		abort();
-	}
+        std::cout << "add deletions as bases  : " << (argAddDelAsBase.getValue()? "yes" : "no") << std::endl; 
+
+        PileupWithGenomeReference<PileupElementBaseQual> pileup(1024, refSeqFileName, argAddDelAsBase.getValue(), inputVCFFileIsGZipped, outputVCFFileIsGZipped);   	
+
+        //process file with index    	
+        if (inputVCFFileName != "")
+        {
+            pileup.processFile(inputBAMFileName, inputBAMIndexFileName, inputVCFFileName, outputVCFFileName);
+        }
+        else
+        {
+            pileup.processFile(inputBAMFileName, outputVCFFileName);
+        }
+    }
+    catch (TCLAP::ArgException &e) 
+    {
+        std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+        abort();
+    }
 
     return(0);
 }
