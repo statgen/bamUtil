83bcc3591a8c8885c4701c30f6971d286f97aaa4
mktrost <mktrost@umich.edu>
2011-03-22 22:38:44 -0400

    Add doxygen comments

--- lib/general/InputFile.h
+++ lib/general/InputFile.h
@@ -14,7 +14,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+/*! \file */ 
 #ifndef __INPUTFILE_H__
 #define __INPUTFILE_H__
 
