2745cb85be29b9dceb14ffd00a60f891e19157cf
mktrost <mktrost@umich.edu>
2011-03-23 20:02:18 -0400

    Revert "Undo accidental removal of karma, metal, recab, and mach."
    This reverts commit 39e3b88e1b5f05a895e72cb497029faf808a63e2.

--- src/karma/DumpInfo.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DUMPINFO_H
-#define _DUMPINFO_H
-
-#include "UserOptions.h"
-#include <istream>
-
-void DumpInfo(CheckArguments a, std::ostream &, const char *file);
-
-#endif
--- src/mach/mach1/Makefile.tool
+++
@@ -1,7 +0,0 @@
-PATH_TO_BASE=../../..
-BIN_DIR=../../bin
-EXE=mach1
-TOOLBASE = 
-SRCONLY = Main.cpp
-USER_LIBS = ../libmach/libmach.a
-USER_INCLUDES = -I../libmach
