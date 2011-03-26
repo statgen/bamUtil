acd7f034a91e1b668b61416fb2eb1e020b64beb5
zhanxw <zhanxw@gmail.com>
2011-03-26 12:09:39 -0400

    Fix header files after using cppclean

--- src/karma/UserOptions.h
+++ src/karma/UserOptions.h
@@ -1,73 +1,74 @@
-/*
- * Copyright (c) 2009 Regents of the University of Michigan
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
+ /*
+  * Copyright (c) 2009 Regents of the University of Michigan
+  *
+  * Permission is hereby granted, free of charge, to any person
+  * obtaining a copy of this software and associated documentation
+  * files (the "Software"), to deal in the Software without
+  * restriction, including without limitation the rights to use,
+  * copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the
+  * Software is furnished to do so, subject to the following
+  * conditions:
+  *
+  * The above copyright notice and this permission notice shall be
+  * included in all copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+  * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+  * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  * OTHER DEALINGS IN THE SOFTWARE.
+  */
 
-#ifndef _USER_OPTIONS
-#define _USER_OPTIONS
+ #ifndef _USER_OPTIONS
+ #define _USER_OPTIONS
 
-#ifndef __STDC_LIMIT_MACROS
-#define __STDC_LIMIT_MACROS
-#endif
+ #ifndef __STDC_LIMIT_MACROS
+ #define __STDC_LIMIT_MACROS
+ #endif
 
-#include <stdint.h>
+ #include <stdint.h>
 
-#include <iostream>
-#include <stdint.h>
-#include <cstring>
-#include <cstdlib>
-#include <vector>
-#include <string>
+ #include <iostream>
+ #include <stdint.h>
+ #include <cstring>
+ #include <cstdlib>
+ #include <vector>
+ #include <string>
 
 
-//
-// base class of a hierarchy of command argument processing.
-//
-// The goal is to minimize redundant argument checking and
-// help message printing, as well as to ensure consistent
-// argument processing among different modes.
-//
-// baseArguments is the base framework plus a few common
-// underlying flags.
-//
-// Derive other classes for other modes and add arguments
-// specific to the mode involved.
-//
+ //
+ // base class of a hierarchy of command argument processing.
+ //
+ // The goal is to minimize redundant argument checking and
+ // help message printing, as well as to ensure consistent
+ // argument processing among different modes.
+ //
+ // baseArguments is the base framework plus a few common
+ // underlying flags.
+ //
+ // Derive other classes for other modes and add arguments
+ // specific to the mode involved.
+ //
 class BaseArguments
 {
- protected:
+  protected:
     std::string optionString;
     virtual void getoptH();
     int argc;
     const char **argv;
     int opt;
- public:
+  public:
     bool debug;
     int seed;
- BaseArguments() : debug(false), seed(12345)
+  BaseArguments() : debug(false), seed(12345)
     {
         optionString += "ds:";
     }
+    virtual ~BaseArguments() {}
     void setArgs(int argc, const char **argv)
     {
         this->argc = argc;
