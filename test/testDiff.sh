e4a7bec898e3ed045750a62a37f0a3c53d5752b7
mktrost <mktrost@umich.edu>
2011-05-16 16:12:30 -0400

    Add tests that have extra records at the beginning and end to ensure they are handled properly.

--- test/testDiff.sh
+++ test/testDiff.sh
@@ -4,8 +4,14 @@
 # Different order/pos on one of the records.
 ../bin/bam diff --in1 testFiles/testDiff1.sam --in2 testFiles/testDiff2.sam --baseQual --out results/diffOrderSam.log 2> results/empty.log && diff results/diffOrderSam.log expected/diffOrderSam.log && diff results/empty.log expected/empty.txt \
 && \
-# Diff order/pos but no threshold for pos
+# Diff order/pos but no threshold for pos.
 ../bin/bam diff --in1 testFiles/testDiff1.sam --in2 testFiles/testDiff2.sam --baseQual --posDiff 1 --out results/diffNoThresh.log 2> results/empty.log && diff results/diffNoThresh.log expected/diffNoThresh.log && diff results/empty.log expected/empty.txt \
+
+# Different order/pos on one of the records reverse the order.
+../bin/bam diff --in1 testFiles/testDiff2.sam --in2 testFiles/testDiff1.sam --baseQual --out results/diffOrderSam2.log 2> results/empty.log && diff results/diffOrderSam2.log expected/diffOrderSam2.log && diff results/empty.log expected/empty.txt \
+&& \
+# Diff order/pos but no threshold for pos reverse the order.
+../bin/bam diff --in1 testFiles/testDiff2.sam --in2 testFiles/testDiff1.sam --baseQual --posDiff 1 --out results/diffNoThresh2.log 2> results/empty.log && diff results/diffNoThresh2.log expected/diffNoThresh2.log && diff results/empty.log expected/empty.txt \
 
 if [ $? -ne 0 ]
 then
