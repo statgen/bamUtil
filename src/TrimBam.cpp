dfe95eac41fcf22c9d5d7445ffa589021b56d255
mktrost <mktrost@umich.edu>
2012-11-14 14:34:47 -0500

    Update TrimBam to have options to trim from either side

--- src/TrimBam.cpp
+++ src/TrimBam.cpp
@@ -20,7 +20,10 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <getopt.h>
 #include "SamFile.h"
+#include "SamFlag.h"
+#include "BgzfFileType.h"
 #include "TrimBam.h"
 
 void TrimBam::trimBamDescription()
@@ -39,6 +42,10 @@ void TrimBam::usage()
 {
     BamExecutable::usage();
     std::cerr << "\t./bam trimBam [inFile] [outFile] [num-bases-to-trim-on-each-side]\n";
+    std::cerr << "Alternately, the number of bases from each side can be specified (either or both -L/-R (--left/--right) can be specified):\n";
+    std::cerr << "\t./bam trimBam [inFile] [outFile] -L [num-bases-to-trim-from-left] -R [num-bases-to-trim-from-right]\n";
+    std::cerr << "By default Left/Right is as the reads are in the SAM/BAM file.\n";
+    std::cerr << "Optionally --reverse/-r can be specified to reverse the left/right for reverese reads\n";
     std::cerr << "trimBam will modify the sequences to 'N', and the quality string to '!'\n";
 }
 
@@ -47,30 +54,113 @@ int TrimBam::execute(int argc, char ** argv)
 {
   SamFile samIn;
   SamFile samOut;
-  int numTrimBase;
+  int numTrimBaseL = 0;
+  int numTrimBaseR = 0;
+  bool noeof = false;
+  bool reverse = false;
+  std::string inName = "";
+  std::string outName = "";
 
-  if ( argc != 5 ) {
+  if ( argc < 5 ) {
     usage();
     abort();
   }
+  inName = argv[2];
+  outName = argv[3];
 
-  if ( ! samIn.OpenForRead(argv[2]) ) {
-      fprintf(stderr, "***Problem opening %s\n",argv[1]);
+  static struct option getopt_long_options[] = {
+      // Input options
+      { "left", required_argument, NULL, 'L'},
+      { "right", required_argument, NULL, 'R'},
+      { "reverse", no_argument, NULL, 'r'},
+      { "noeof", no_argument, NULL, 'n'},
+      { NULL, 0, NULL, 0 },
+  };
+  
+  int argIndex = 4;
+  if(argv[argIndex][0] != '-')
+  {
+      // This is the number of bases to trim off both sides
+      // so convert to a number.
+      numTrimBaseL = atoi(argv[argIndex]);
+      numTrimBaseR = numTrimBaseL;
+      ++argIndex;
+  }
+
+  int c = 0;
+ int n_option_index = 0;
+  // Process any additional parameters
+  while ( ( c = getopt_long(argc, argv,
+                            "L:R:rn", getopt_long_options, &n_option_index) )
+          != -1 )
+  {
+      switch(c) 
+      {
+          case 'L':
+              numTrimBaseL = atoi(optarg);
+              break;
+          case 'R':
+              numTrimBaseR = atoi(optarg);
+              break;
+          case 'r':
+              reverse = true;
+              break;
+          case 'n':
+              noeof = true;
+              break;
+          default:
+              fprintf(stderr,"Unrecognized option %s",
+                      getopt_long_options[n_option_index].name);
+              abort();
+      }
+  }
+
+  if(noeof)
+  {
+      // Set that the eof block is not required.
+      BgzfFileType::setRequireEofBlock(false);
+  }
+
+  if ( ! samIn.OpenForRead(inName.c_str()) ) {
+      fprintf(stderr, "***Problem opening %s\n",inName.c_str());
     abort();
   }
 
-  if(!samOut.OpenForWrite(argv[3])) {
+  if(!samOut.OpenForWrite(outName.c_str())) {
     fprintf(stderr, "%s\n", samOut.GetStatusMessage());
     return(samOut.GetStatus());
   }
   
-  numTrimBase = atoi(argv[4]);
-
   fprintf(stderr,"Arguments in effect: \n");
-  fprintf(stderr,"\tInput file : %s\n",argv[2]);
-  fprintf(stderr,"\tOutput file : %s\n",argv[3]);
-  fprintf(stderr,"\t#TrimBases : %d\n",numTrimBase);
-  
+  fprintf(stderr,"\tInput file : %s\n",inName.c_str());
+  fprintf(stderr,"\tOutput file : %s\n",outName.c_str());
+  if(numTrimBaseL == numTrimBaseR)
+  {
+      fprintf(stderr,"\t#Bases to trim from each side : %d\n", numTrimBaseL);
+  }
+  else
+  {
+      fprintf(stderr,"\t#Bases to trim from the left of forward strands : %d\n",
+              numTrimBaseL);
+      fprintf(stderr,"\t#Bases to trim from the right of forward strands: %d\n",
+              numTrimBaseR);
+      if(reverse)
+      {
+          // When using the reverse option, reverse strands are treated the opposite.
+          fprintf(stderr,"\t#Bases to trim from the left of reverse strands : %d\n",
+                  numTrimBaseR);
+          fprintf(stderr,"\t#Bases to trim from the right of reverse strands : %d\n",
+                  numTrimBaseL);
+      }
+      else
+      {
+          fprintf(stderr,"\t#Bases to trim from the left of reverse strands : %d\n",
+                  numTrimBaseL);
+          fprintf(stderr,"\t#Bases to trim from the right of reverse strands : %d\n",
+                  numTrimBaseR);
+      }
+  }
+ 
    // Read the sam header.
    SamFileHeader samHeader;
    if(!samIn.ReadHeader(samHeader))
@@ -96,6 +186,22 @@ int TrimBam::execute(int argc, char ** argv)
      // Successfully read a record from the file, so write it.
      strcpy(seq,samRecord.getSequence());
      strcpy(qual,samRecord.getQuality());
+
+     // Number of bases to trim from the left/right,
+     // set based on reverse flag and strand info.
+     int trimLeft = numTrimBaseL;
+     int trimRight = numTrimBaseR;
+     if(reverse)
+     {
+         if(SamFlag::isReverse(samRecord.getFlag()))
+         {
+             // We are reversing the reverse reads,
+             // so swap the left & right trim counts.
+             trimRight = numTrimBaseL;
+             trimLeft = numTrimBaseR;
+         }
+     }
+
      len = strlen(seq);
      // Do not trim if sequence is '*'
      if ( strcmp(seq, "*") != 0 ) {
@@ -109,7 +215,9 @@ int TrimBam::execute(int argc, char ** argv)
          fprintf(stderr,"ERROR: Sequence and Quality have different length\n");
          abort();
        }
-       if ( len < numTrimBase ) {
+       if ( len < (trimLeft + trimRight) ) {
+         // Read Length is less than the total number of bases to trim,
+         // so trim the entire read.
          for(i=0; i < len; ++i) {
            seq[i] = 'N';
            if ( qualValue ) {
@@ -117,15 +225,27 @@ int TrimBam::execute(int argc, char ** argv)
            }
          }
        }
-       else {
-         for(i=0; i < numTrimBase; ++i) {
-           seq[i] = 'N';
-           seq[len-i-1] = 'N';
-           if ( qualValue ) {
-             qual[i] = '!';
-             qual[len-i-1] = '!';
+       else
+       {
+           // Read Length is larger than the total number of bases to trim,
+           // so trim from the left, then from the right.
+           for(i=0; i < trimLeft; ++i)
+           {
+               // Trim the bases from the left.
+               seq[i] = 'N';
+               if ( qualValue )
+               {
+                   qual[i] = '!';
+               }
            }
-         }
+           for(i = 0; i < trimRight; i++)
+           {
+               seq[len-i-1] = 'N';
+               if(qualValue)
+               {
+                   qual[len-i-1] = '!';
+               }
+           }
        }
        samRecord.setSequence(seq);
        samRecord.setQuality(qual);
--- test/testTrimBam.sh
+++ test/testTrimBam.sh
@@ -1,6 +1,6 @@
 ERROR=false
 
-../bin/bam trimBam testFilesLibBam/testSam.sam results/trimSam.sam 2 2> results/testTrim.log
+../bin/bam trimBam testFiles/testTrim.sam results/trimSam.sam 2 2> results/testTrim.log
 if [ $? -ne 0 ]
 then
     ERROR=true
@@ -12,8 +12,47 @@ then
     ERROR=true
 fi
 
+../bin/bam trimBam testFiles/testTrim.sam results/trimSam3.sam 3 2> results/testTrim3.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
+diff results/trimSam3.sam expected/trimSam3.sam && diff results/testTrim3.log expected/testTrim3.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
+
+../bin/bam trimBam testFiles/testTrim.sam results/trimSamL1R2.sam -L 1 -R 2 2> results/testTrimL1R2.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
+diff results/trimSamL1R2.sam expected/trimSamL1R2.sam && diff results/testTrimL1R2.log expected/testTrimL1R2.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
+
+../bin/bam trimBam testFiles/testTrim.sam results/trimSamL1R2rev.sam -r -L 1 -R 2 2> results/testTrimL1R2rev.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
+diff results/trimSamL1R2rev.sam expected/trimSamL1R2rev.sam && diff results/testTrimL1R2rev.log expected/testTrimL1R2rev.log
+if [ $? -ne 0 ]
+then
+    ERROR=true
+fi
+
 
 if($ERROR == true)
 then
+  echo "fail testTrimBam.sh"
   exit 1
 fi
